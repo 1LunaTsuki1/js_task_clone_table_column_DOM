64eb0cd7fd87664799e3e6ce8aba20fc9460b747
Luna <yanka16247@mail.com>
2025-09-10 17:13:53 +0300

    add task solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,10 @@ const table = document.querySelector('table');
 const rows = [...table.rows];
 
 rows.forEach((row) => {
+  if (row.cells.length < 2) {
+    return;
+  }
+
   const newCell = row.cells[1].cloneNode(true);
 
   row.lastElementChild.before(newCell);
